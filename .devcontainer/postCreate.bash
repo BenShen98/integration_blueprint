05a2cb60fad19118e9d005498650e7552bc94493
docker <docker@example.com>
2022-06-12 21:19:00 +0000

    tst2

--- .devcontainer/postCreate.bash
+++ .devcontainer/postCreate.bash
@@ -2,16 +2,22 @@
 set +e
 
 # normal postCreateCommand
-container install
+# container install
+python3 -m pip --disable-pip-version-check install --upgrade git+https://github.com/home-assistant/home-assistant.git@dev
 
-# fix symbolic link to custom_components
-rm /config/custom_components
-mkdir /config/custom_components
+# setup /config
+hass -c /config --script ensure_config
+mkdir -p /config/custom_components
 touch /config/custom_components/__init__.py
-ln -s /workspaces/integration_blueprint/custom_components/integration_blueprint /config/custom_components/
+ln -sf /workspaces/integration_blueprint/custom_components/integration_blueprint /config/custom_components/
+ln -sf /workspaces/integration_blueprint/.devcontainer/configuration.yaml /config
 
 # add pyscript custom_components
 cd /workspaces && git clone https://github.com/custom-components/pyscript.git --depth 1
 ln -s /workspaces/pyscript/custom_components/pyscript /config/custom_components/
 pip install hass_pyscript_kernel
 jupyter pyscript install
+
+# MISC
+git config --global user.email "docker@example.com"
+git config --global user.name "docker"
