903648298063ecac8335acec8974ddf52a4aeb8c
docker <docker@example.com>
2022-06-12 22:35:36 +0000

    check

--- .devcontainer/postCreate.bash
+++ .devcontainer/postCreate.bash
@@ -1,9 +1,14 @@
 #!/bin/bash
 set +e
 
+# apt updates
+apt update
+apt install libffi-dev libssl-dev libjpeg-dev zlib1g-dev autoconf build-essential libopenjp2-7 libtiff5 libturbojpeg0-dev tzdata
+
 # normal postCreateCommand
 # container install
 python3 -m pip --disable-pip-version-check install --upgrade git+https://github.com/home-assistant/home-assistant.git@dev
+python3 -m pip install notebook
 
 # setup /config
 hass -c /config --script ensure_config
@@ -17,6 +22,10 @@ cd /workspaces && git clone https://github.com/custom-components/pyscript.git --
 ln -s /workspaces/pyscript/custom_components/pyscript /config/custom_components/
 pip install hass_pyscript_kernel
 jupyter pyscript install
+
+# notebooks
+mkdir -p /workspaces/notebooks
+wget https://raw.githubusercontent.com/craigbarratt/hass-pyscript-jupyter/master/pyscript_tutorial.ipynb -P /workspaces/notebooks/
 
 # MISC
 git config --global user.email "docker@example.com"
